e784b049f7498350bd64d6761a3fc6e425195ae0
honux <honux@naver.com>
2014-11-20 10:43:51 +0900

    modify copy.sh for osx

--- copy.sh
+++ copy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
 WWW=/var/www
 # echo $#
 if [ $# -eq 1 ]; then
 	WWW=$1
+fi
+if [[ $1 == beta ]]; then
+	WWW=/var/beta
+	echo "You choose BETA directory"
+elif [[ $1 == osx ]]; then
+	WWW=//Library/WebServer/Documents
 fi
 echo remove  all files to $WWW
 sudo rm -rf $WWW/*
